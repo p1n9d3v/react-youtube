b88e8ae19702c6f9c88cc82dd1838f794ee87373
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-01-04 01:04:35 +0900

    fix(Video): Add controls parameter

--- src/components/ui/Video/index.jsx
+++ src/components/ui/Video/index.jsx
@@ -1,12 +1,12 @@
 import YouTube from 'react-youtube';
 
-function Video({ vid, autoplay = 0 }) {
+function Video({ vid, autoplay = 0, controls = 0 }) {
     const opts = {
         height: '100%',
         width: '100%',
         autoplay,
         playerVars: {
-            controls: 0,
+            controls,
             modestbranding: 1,
         },
     };
