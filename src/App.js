15e821db0e1b289e53de0c5e925c1979f0f4c478
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-01-03 17:43:37 +0900

    style: setting reset css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <button>1</button>
+        </div>
+    );
 }
 
 export default App;
